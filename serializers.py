4b39b3ceab1f73d3876dad00a263c915db7e00a1
hamzaimran7 <140632210+hamzaimran7@users.noreply.github.com>
2024-10-28 05:55:29 -0400

    Add files via upload

---
+++ serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import student
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=student
+        fields=(
+            'name','age'
+        )
